1d6ee1dcc48900bb4ff1b0a1ca04f0e60ca3a89a
jlyheden <johan@lyheden.com>
2014-05-03 14:48:06 +0200

    maildir_path fixes

--- dovecot/compress_maildir.sh
+++ dovecot/compress_maildir.sh
@@ -32,7 +32,7 @@ for mail_file_path in $tocompress; do
   # Get file name of mail
   mail_file_name=$(basename "$mail_file_path")
   tmp_file_path="$tmp_dir/$mail_file_name"
-  maildir_path=$(dirname "$(dirname \"$mail_file_name\")")
+  maildir_path=$(dirname "$(dirname \"$mail_file_path\")")
   
   c_logger "Processing file: $mail_file_path"
   c_logger "mail_file_name: $mail_file_name"
