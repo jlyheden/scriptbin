e1fe0910edd7eaeceff5f49dbcce60dd0741f8c8
jlyheden <johan@lyheden.com>
2014-05-03 14:44:47 +0200

    log to file

--- dovecot/compress_maildir.sh
+++ dovecot/compress_maildir.sh
@@ -10,15 +10,19 @@ fi
 
 tmp_dir="/tmp/maildir_compress/$1"
 
+c_logger() {
+  echo "${date}: $1" >> $tmp_dir/compress.log
+}
+
 if [ -d "$tmp_dir" ]; then
-  echo "Clearing temp dir: $tmp_dir"
+  c_logger "Clearing temp dir: $tmp_dir"
   rm -rf "$tmp_dir"
 fi
 
-echo "Creating temp dir: $tmp_dir"
+c_logger "Creating temp dir: $tmp_dir"
 mkdir -p "$tmp_dir"
 
-echo "Scanning $1 for non-gzipped files"
+c_logger "Scanning $1 for non-gzipped files"
 tocompress=$(find $1 -name "*,S=*" -printf "file -b '%p' |grep -qs ^gzip || echo '%p'\n" | sh)
 
 # Iterate on newline, not whitespace
@@ -27,16 +31,19 @@ IFS='
 
 for mail_file_path in $tocompress; do
 
-  echo "Processing file: $mail_file_path"
-
   # Get file name of mail
   mail_file_name=$(basename "$mail_file_path")
   tmp_file_path="$tmp_dir/$mail_file_name"
   maildir_path=$(dirname "$(dirname \"$mail_file_name\")")
+  
+  c_logger "Processing file: $mail_file_path"
+  c_logger "mail_file_name: $mail_file_name"
+  c_logger "tmp_file_path: $tmp_file_path"
+  c_logger "maildir_path: $maildir_path"
 
   # Die if tmp file already exists
   if [ -f "$tmp_file_path" ]; then
-    echo "The tmp file $tmp_file_path already exists!"
+    c_logger "The tmp file $tmp_file_path already exists!"
     exit 1
   fi
 
@@ -50,7 +57,7 @@ for mail_file_path in $tocompress; do
 
   # Lock maildir
   #PID=$(/usr/lib/dovecot/maildirlock "$maildir_path" 20)
-  echo mv "$tmp_file_path" "$mail_file_path"
+  c_logger mv "$tmp_file_path" "$mail_file_path"
   #kill $PID
 
 done
